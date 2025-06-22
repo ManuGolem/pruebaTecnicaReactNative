d3a1e081d3ff4ee88747b210a77e6941e7980c27
ManuGolem <manuelcarreras2013@gmail.com>
2025-06-21 22:13:18 -0300

    quito valores negativos

--- componentes/BotonesCantidad.js
+++ componentes/BotonesCantidad.js
@@ -3,11 +3,11 @@ import { View, Text, Pressable } from "react-native";
 export default function BotonesCantidad({ cantidad, setCantidad, styles }) {
     return (
         <View style={styles.cantidad}>
-            <Pressable style={styles.botones} onPress={() => setCantidad(cantidad - 1)}>
+            <Pressable style={styles.botones} onPress={() => setCantidad((prev) => Math.max(0, prev - 1))}>
                 <Text style={{ fontSize: 20 }}>-</Text>
             </Pressable>
             <Text style={{ fontSize: 24, fontWeight: "bold" }}>{cantidad}</Text>
-            <Pressable style={styles.botones} onPress={() => setCantidad(cantidad + 1)}>
+            <Pressable style={styles.botones} onPress={() => setCantidad((prev) => prev + 1)}>
                 <Text style={{ fontSize: 20 }}>+</Text>
             </Pressable>
         </View>
